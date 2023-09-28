26f41cad90de1354003ba36addeb985a7f683f49
igalukhin <igaluhin@gmaol.com>
2023-09-28 12:54:11 +0300

    Add fixed lib pytube. Test pipelines. Change project structure

---
+++ data_pipelines/index_pipeline.py
@@ -0,0 +1,144 @@
+import os
+import json
+from dataclasses import dataclass
+from typing import List
+from dotenv import load_dotenv
+import openai
+from llama_index import Document
+from llama_index.node_parser import SimpleNodeParser
+from llama_index import StorageContext, load_index_from_storage
+from llama_index import VectorStoreIndex, ServiceContext
+from data_pipelines.parser_transcribe import ParserTranscribe, get_video_urls
+
+load_dotenv()
+openai.api_key = os.getenv("API_KEY")
+
+@dataclass()
+class IndexPipeline:
+    """
+    Запускает пайплайн получения индекса -
+    от определения новых видео в YouTube до сохранения индекса.
+    """
+    path_to_save: str
+    url_file_path: str
+    json_video_info_path: str
+    index_folder: str
+    chunk_size: int = 200
+    chunk_overlap: int = 50
+
+    def _get_download_urls(self, channel_url) -> List[str]:
+        """Определяет список видео для скачивания"""
+
+        # Determine the set of downloaded videos
+        downloaded_videos = set()
+        if os.path.exists(self.url_file_path):
+            with open(self.url_file_path, "r", encoding="utf-8") as f:
+                for line in f:
+                    downloaded_videos.add(line.strip())
+
+        # Determine the set of videos to download
+        new_videos = list(set(get_video_urls(channel_url)) - downloaded_videos)
+
+        # Add new videos to the file
+        with open(self.url_file_path, "a", encoding="utf-8") as f:
+            for video in new_videos:
+                f.write(video + "\n")
+
+        return new_videos
+
+    def _transcribe_videos(self, new_videos: List[str]) -> None:
+        """
+        Скачивает и транскрибирует видео из списка self.new_videos.
+        Текст и метаданные сохраняются в json-файл.
+        """
+        transcriber = ParserTranscribe(self.path_to_save, self.json_video_info_path)
+        for url in new_videos:
+            transcriber.get_transcribe_video(url)
+
+    def _get_index(self, new_videos: List[str]) -> None:
+        """
+        Функция должна работать следующим образом:
+
+        1. Если есть сохраненный индекс в self.storage_index_path -
+        загружает его.
+        2. По списку new_videos находит документы в json и добавляет их в индекс.
+        Или создает новый индекс, если self.storage_index_path не существует
+        3. Сохраняет индекс
+        """
+
+        # Загружаем индекс
+        vector_store_path = os.path.join(self.index_folder, "vector_store.json")
+        if os.path.exists(vector_store_path):
+            storage_context = StorageContext.from_defaults(persist_dir=self.index_folder)
+            index = load_index_from_storage(storage_context)
+        else:
+            # Или создаем пустой
+            node_parser = SimpleNodeParser.from_defaults(
+                chunk_size=self.chunk_size,
+                chunk_overlap=self.chunk_overlap
+            )
+            service_context = ServiceContext.from_defaults(node_parser=node_parser)
+            index = VectorStoreIndex([], service_context=service_context)
+
+        # Выбираем документы для добавления в индекс
+        with open(self.json_video_info_path, "r", encoding="utf-8") as f:
+            data_json = json.load(f)
+        to_download_data = []
+        for i in new_videos:
+            for j in data_json:
+                if i == j["url"][0]:
+                    to_download_data.append(j)
+
+        # Формируем документы
+        documents = [
+            Document(
+                text=data["text"][0],
+                metadata={"url": data["url"][0], "title": data["title"][0]},
+            )
+            for data in to_download_data
+        ]
+
+        # Добавляем документы в индекс
+        for doc in documents:
+            index.insert(doc)
+
+        # Сохраняем индекс
+        index.storage_context.persist(self.index_folder)
+
+
+    def run(self, channel_url: str, test: bool=False) -> None:
+        """Запускает пайплайн получения индекса"""
+        new_videos = self._get_download_urls(channel_url)
+        if new_videos:
+            if test:
+                new_videos = new_videos[:2]
+            self._transcribe_videos(new_videos)
+            self._get_index(new_videos)
+        else:
+            print("No new videos to download")
+
+if __name__ == "__main__":
+    pipe = IndexPipeline(
+        "../data/audio",
+        "../data/urls_of_channel_videos.txt",
+        "../data/video_info.json",
+        "../data/index_storage"
+    )
+    pipe.run("https://www.youtube.com/c/karpovcourses", test=True)
+    storage_cntxt = StorageContext.from_defaults(persist_dir="../data/index_storage")
+    idx = load_index_from_storage(storage_cntxt)
+    query_engine = idx.as_query_engine(
+        include_text=True,
+        response_mode="no_text",
+        embedding_mode="hybrid",
+        similarity_top_k=3,
+    )
+    QUESTION = "Какие софт скиллы нужны для дата саентиста"
+    retrival = query_engine.query(
+        QUESTION,
+    )
+    print(f"Q: {QUESTION}")
+    information = [
+        (i.text, i.metadata["url"], i.metadata["title"]) for i in retrival.source_nodes
+    ]
+    print(information)
---
+++ data_pipelines/parser_transcribe.py
@@ -0,0 +1,231 @@
+import os
+import json
+import subprocess
+import time
+from dataclasses import dataclass
+from typing import List
+from dotenv import load_dotenv
+import openai
+from pytube import Channel
+from pytube import YouTube
+
+load_dotenv()
+openai.api_key = os.getenv("API_KEY")
+
+@dataclass()
+class ParserTranscribe:
+    """
+    A pipeline for downloading and transcribing audio from YouTube videos.
+    Has two public methods:
+    - get_transcribe_video(url_of_video: str) - downloads and transcribes audio
+    and saves data to a json file
+    - get_video_urls(channel_url: str) - get list of all video urls from youtube-channel
+    """
+    path_to_save: str # Путь к папке с аудио
+    json_video_info_path: str # Путь к json-файлу
+    segment_time: int = 900 # Длительность сегмента при нарезке аудио
+    max_attempts: int = 5  # максимальное количество попыток
+    delay: int = 10  # задержка между попытками в секундах
+
+
+    def _get_video_info(self, video_url: str) -> dict:
+        """
+        Get info from Youtube-video - title, description, audio track
+        Parameters
+        ----------
+        url: str
+          url of Youtube-video
+
+        Returns
+        -------
+        dict
+          keys:
+          - "url": str
+          - "title": str
+          - "description": str
+          - "audio_path": str
+            saved audio path
+        """
+        yt = YouTube(video_url)
+        audio_name = yt.video_id + ".mp4"
+        audio_path = os.path.join(self.path_to_save, audio_name)
+        title = yt.title
+        print(f"Downloading {title}...")
+        # The next string 'yt.streams...' must be before the following.
+        # Surprisingly, but the fact is, if this is not observed,
+        # then it is impossible to get a description.
+        # Download mp4 audio file:
+        yt.streams.filter(only_audio=True).first().download(self.path_to_save, audio_name)
+        description = yt.description
+        return {
+            "url": video_url,
+            "title": title,
+            "description": description,
+            "audio_path": audio_path
+        }
+
+    def _download_channel_audio_track(self, url_of_video: str) -> str:
+        """
+        Download audio track from YouTube-video and save info to json.
+        Return path to downloaded audio track
+        """
+        if os.path.exists(self.json_video_info_path):
+            with open(self.json_video_info_path, "r", encoding="utf-8") as f:
+                video_info = json.load(f)
+        else:
+            video_info = []
+        url_info = self._get_video_info(url_of_video)
+        print(f"Path to mp4 file: {url_info['audio_path']}\n")
+
+        video_info_item = {"url": [], "title": [], "description": [], "audio_path": [], "text": []}
+        for key, value in url_info.items():
+            video_info_item[key].append(value)
+
+        # Проверяем, что такого url в json нет
+        exist_url = True
+        if video_info:
+            for itm in video_info:
+                exist_url = exist_url and (itm["url"][0] != video_info_item["url"][0])
+        if exist_url:
+            video_info.append(video_info_item)
+
+        with open(self.json_video_info_path, "w", encoding="utf-8") as f:
+            json.dump(video_info, f, ensure_ascii=False, indent=4)
+
+        return video_info_item["audio_path"][0]
+
+    def _split_audio(self, file_path, output_pattern):
+        """
+        Разделяет аудиофайл на сегменты с использованием утилиты ffmpeg.
+
+        Parameters
+        ----------
+        file_path : str
+            Путь к исходному аудиофайлу, который необходимо разделить.
+        output_pattern : str
+            Шаблон имени выходного файла. Должен содержать `%03d` для нумерации сегментов
+            (например, "output_segment%03d.mp3").
+
+        Returns
+        -------
+        None
+
+        """
+        cmd = [
+            "ffmpeg",
+            "-i",
+            file_path,
+            "-f",
+            "segment",
+            "-segment_time",
+            str(self.segment_time),
+            "-c:a",
+            "libmp3lame",
+            output_pattern,
+        ]
+        subprocess.run(cmd, check=False)
+
+    def _transcribe_with_whisper(self, audio_path):
+        """
+        Транскрибирует аудиофайл с использованием модели Whisper от OpenAI.
+
+        Функция пытается транскрибировать аудио многократно
+        (до max_attempts раз) в случае ошибок API.
+
+        Parameters
+        ----------
+        audio_path : str
+            Путь к аудиофайлу, который необходимо транскрибировать.
+
+        Returns
+        -------
+        str
+            Транскрибированный текст аудиофайла.
+
+        Raises
+        ------
+        openai.error.APIError
+            Если после всех попыток транскрибировать аудио возникает ошибка.
+
+        """
+
+        for attempt in range(self.max_attempts):
+            try:
+                with open(audio_path, "rb") as audio_file:
+                    transcript = openai.Audio.transcribe(
+                        file=audio_file,
+                        model="whisper-1",
+                        response_format="text",
+                        language=["ru"],
+                        prompt="Это организация Karpov Courses. Машинное обучение,"
+                               " Machine learning, Дата саентист, data scientist,"
+                               " dev ops, девопс, stepik.org, степик",
+                    )
+                return transcript
+            except openai.error.APIError as e:
+                print(f"Ошибка при обращении к API (попытка {attempt + 1}): {e}")
+                if attempt < self.max_attempts - 1:  # если это не последняя попытка
+                    print(f"Ожидание {self.delay} секунд перед следующей попыткой...")
+                    time.sleep(self.delay)
+                else:
+                    print("Превышено максимальное количество попыток.")
+                    raise  # повторно вызываем исключение, чтобы сообщить о проблеме
+        return None
+
+    def _get_transcribe(self, audio_path: str):
+        file_name = os.path.basename(audio_path)
+        if file_name.endswith(".mp4"):
+            file_path = os.path.join(self.path_to_save, file_name)
+            output_pattern = os.path.join(
+                self.path_to_save, f"{file_name[:-4]}_segment%03d.mp3"
+            )
+            self._split_audio(file_path, output_pattern)
+
+            # Собираем все транскрибации для данного файла
+            transcriptions = []
+            segment_files = sorted(
+                [
+                    f
+                    for f in os.listdir(self.path_to_save)
+                    if f.startswith(file_name[:-4] + "_segment")
+                ]
+            )
+            for segment_file in segment_files:
+                if segment_file.startswith(file_name[:-4] + "_segment"):
+                    segment_path = os.path.join(self.path_to_save, segment_file)
+                    transcriptions.append(self._transcribe_with_whisper(segment_path))
+                    os.remove(
+                        segment_path
+                    )  # Удаляем спличенный файл после транскрибации
+            os.remove(audio_path) # Удаляем исходный файл
+
+            # Сохраняем все транскрибации в один текстовый файл
+            print(transcriptions)
+            with open(self.json_video_info_path, "r", encoding="utf-8") as f:
+                data_list = json.load(f)
+
+            for item in data_list:
+                if item["audio_path"][0] == file_path:
+                    item["text"] = transcriptions
+                    break
+
+            with open(self.json_video_info_path, "w", encoding="utf-8") as f:
+                json.dump(data_list, f, ensure_ascii=False, indent=4)
+
+    def get_transcribe_video(self, url_of_video: str):
+        """Основная функция для полного запуска пайплайна транскрибации видео по ссылке"""
+        audio_path = self._download_channel_audio_track(url_of_video)
+        self._get_transcribe(audio_path)
+
+def get_video_urls(channel_url: str) -> List[str]:
+    """Get list of all video urls from youtube-channel"""
+    channel = Channel(channel_url)
+    return channel.video_urls
+
+if __name__ == "__main__":
+    parser = ParserTranscribe(
+        "../data/audio",
+        "../data/video_info.json"
+    )
+    parser.get_transcribe_video("https://www.youtube.com/watch?v=OXtOhjeiTzw")
+    parser.get_transcribe_video("https://www.youtube.com/watch?v=9W1v-DkXriY")
